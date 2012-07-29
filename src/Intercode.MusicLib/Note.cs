c4ed05b5228a63dcc823dbe25cf1ba40bf2eaa13
Eddie Velasquez <evelasquez@intercodeconsulting.com>
2012-07-29 08:11:56 -0700

    Converted Note from a class to a struct

--- src/Intercode.MusicLib/Note.cs
+++ src/Intercode.MusicLib/Note.cs
@@ -3,23 +3,22 @@
   using System;
   using System.Diagnostics.Contracts;
 
-  public class Note: IEquatable<Note>
+  public struct Note: IEquatable<Note>
   {
-    #region Constants
-
     private const int INTERVAL_COUNT = 12;
     private const int MIN_OCTAVE = 1;
     private const int MAX_OCTAVE = 8;
-
-    #endregion
-
-    #region Data Members
 
     private static readonly string[] s_sharpRepresentations = new[]
     { "A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#" };
 
     private static readonly string[] s_flatRepresentations = new[]
     { "A", "Bb", "B", "C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab" };
+
+    #region Data Members
+
+    private readonly byte _index;
+    private readonly Accidental _accidental;
 
     #endregion
 
@@ -34,20 +33,97 @@
       Contract.Requires<ArgumentOutOfRangeException>(accidental >= Accidental.None, "accidental");
       Contract.Requires<ArgumentOutOfRangeException>(accidental <= Accidental.Flat, "accidental");
 
-      Index = (byte)(((octave - 1) * INTERVAL_COUNT) + tone);
-      Accidental = accidental;
+      _index = (byte)(((octave - 1) * INTERVAL_COUNT) + tone);
+      _accidental = accidental;
     }
 
     [ Obsolete ]
     private Note( byte index, Accidental accidental )
     {
-      Index = index;
-      Accidental = accidental;
+      _index = index;
+      _accidental = accidental;
     }
 
     #endregion
 
-    #region Factories
+    #region Properties
+
+    private byte Index
+    {
+      get { return _index; }
+    }
+
+    public Int32 Octave
+    {
+      get { return (Index / INTERVAL_COUNT) + 1; }
+    }
+
+    public Tone Tone
+    {
+      get { return (Tone)(Index % INTERVAL_COUNT); }
+    }
+
+    public Accidental Accidental
+    {
+      get { return _accidental; }
+    }
+
+    #endregion
+
+    #region IEquatable<Note> Members
+
+    public bool Equals( Note other )
+    {
+      return Index == other.Index;
+    }
+
+    #endregion
+
+    #region Overrides
+
+    public override bool Equals( object obj )
+    {
+      if( ReferenceEquals(null, obj) )
+        return false;
+
+      return obj is Note && Equals((Note)obj);
+    }
+
+    public override int GetHashCode()
+    {
+      return Index.GetHashCode();
+    }
+
+    public override string ToString()
+    {
+      int pos = Index % INTERVAL_COUNT;
+      if( Accidental == Accidental.Flat )
+        return s_flatRepresentations[pos] + Octave;
+
+      return s_sharpRepresentations[pos] + Octave;
+    }
+
+    #endregion
+
+    #region Implementation
+
+    private static int CalcOctave( int index )
+    {
+      int octave = (index / INTERVAL_COUNT) + 1;
+      return octave;
+    }
+
+    private static Accidental CalcAccidental( int index, bool flat )
+    {
+      var newAccidental = Accidental.None;
+      int pos = index % INTERVAL_COUNT;
+      if( s_sharpRepresentations[pos].Length > 1 )
+        newAccidental = flat ? Accidental.Flat : Accidental.Sharp;
+
+      return newAccidental;
+    }
+
+    #endregion
 
     public static Note A( int octave )
     {
@@ -185,28 +261,6 @@
       return new Note(Tone.AFlat, Accidental.Flat, octave);
     }
 
-    #endregion
-
-    #region Properties
-
-    private byte Index { get; set; }
-
-    public Int32 Octave
-    {
-      get { return (Index / INTERVAL_COUNT) + 1; }
-    }
-
-    public Tone Tone
-    {
-      get { return (Tone)(Index % INTERVAL_COUNT); }
-    }
-
-    public Accidental Accidental { get; private set; }
-
-    #endregion
-
-    #region Public Methods
-
     public Note AsSharp()
     {
       if( Accidental == Accidental.None )
@@ -224,10 +278,6 @@
       var note = new Note(Tone, Accidental.Flat, Octave);
       return note;
     }
-
-    #endregion
-
-    #region Internal Methods
 
     internal Note Next( int interval, bool flat )
     {
@@ -266,83 +316,14 @@
       return note;
     }
 
-    #endregion
-
-    #region Overrides
-
-    #region Equality members
-
-    public bool Equals( Note other )
-    {
-      if( ReferenceEquals(null, other) )
-        return false;
-
-      if( ReferenceEquals(this, other) )
-        return true;
-
-      return Index == other.Index;
-    }
-
-    public override bool Equals( object obj )
-    {
-      if( ReferenceEquals(null, obj) )
-        return false;
-
-      if( ReferenceEquals(this, obj) )
-        return true;
-
-      if( obj.GetType() != GetType() )
-        return false;
-
-      return Equals((Note)obj);
-    }
-
-    public override int GetHashCode()
-    {
-      return Index.GetHashCode();
-    }
-
     public static bool operator ==( Note left, Note right )
     {
-      return Equals(left, right);
+      return left.Equals(right);
     }
 
     public static bool operator !=( Note left, Note right )
     {
-      return !Equals(left, right);
+      return !left.Equals(right);
     }
-
-    #endregion
-
-    public override string ToString()
-    {
-      int pos = Index % INTERVAL_COUNT;
-      if( Accidental == Accidental.Flat )
-        return s_flatRepresentations[pos] + Octave;
-
-      return s_sharpRepresentations[pos] + Octave;
-    }
-
-    #endregion
-
-    #region Implementation
-
-    private static int CalcOctave( int index )
-    {
-      int octave = (index / INTERVAL_COUNT) + 1;
-      return octave;
-    }
-
-    private static Accidental CalcAccidental( int index, bool flat )
-    {
-      var newAccidental = Accidental.None;
-      int pos = index % INTERVAL_COUNT;
-      if( s_sharpRepresentations[pos].Length > 1 )
-        newAccidental = flat ? Accidental.Flat : Accidental.Sharp;
-
-      return newAccidental;
-    }
-
-    #endregion
   }
 }
