26580e2ee53eabe4d30720cf5334e81d700ca9c6
Eddie Velasquez <evelasquez@intercodeconsulting.com>
2012-07-28 17:04:27 -0700

    Implementing the Scale class

---
+++ src/Intercode.MusicLib.Test/ScaleTest.cs
@@ -0,0 +1,31 @@
+﻿namespace Intercode.MusicLib.Test
+{
+  using System.Linq;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+  [ TestClass ]
+  public class ScaleTest
+  {
+    [ TestMethod ]
+    public void ConstructorTest()
+    {
+      var major = new Scale("Major", 2, 2, 1, 2, 2, 2);
+      Assert.AreEqual("Major", major.Name);
+    }
+
+    [ TestMethod ]
+    public void MajorGetNotesTest()
+    {
+      TestScale(Scale.Major, Note.C1, Note.C1, Note.D1, Note.E1, Note.F1, Note.G1, Note.A2, Note.B2);
+    }
+
+    private static void TestScale( Scale scale, Note root, params Note[] expectedNotes )
+    {
+      Note[] notes = scale.GetNotes(root).ToArray();
+      Assert.AreEqual(expectedNotes.Length, notes.Length);
+
+      for( int i = 0; i < expectedNotes.Length; i++ )
+        Assert.AreEqual(expectedNotes[i], notes[i]);
+    }
+  }
+}
---
+++ src/Intercode.MusicLib/Scale.cs
@@ -0,0 +1,48 @@
+﻿namespace Intercode.MusicLib
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Diagnostics.Contracts;
+
+  public class Scale
+  {
+    #region Constants
+
+    public static readonly Scale Major = new Scale("Major", 2, 2, 1, 2, 2, 2);
+
+    #endregion
+
+    #region Construction
+
+    public Scale( string name, params int[] intervals )
+    {
+      Contract.Requires<ArgumentNullException>(name != null, "name");
+      Contract.Requires<ArgumentException>(name.Length > 0, "name");
+      Contract.Requires<ArgumentException>(intervals.Length > 0, "intervals");
+
+      Name = name;
+      Intervals = intervals;
+    }
+
+    #endregion
+
+    #region Properties
+
+    public String Name { get; private set; }
+    private Int32[] Intervals { get; set; }
+
+    #endregion
+
+    public IEnumerable<Note> GetNotes( Note root )
+    {
+      yield return root;
+
+      Note current = root;
+      foreach( var interval in Intervals )
+      {
+        current = current.Next(interval, false);
+        yield return current;
+      }
+    }
+  }
+}
