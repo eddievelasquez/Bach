830452df9994703b012b4885eaee56abf11d9d2b
Eddie Velasquez <evelasquez@intercodeconsulting.com>
2013-09-03 11:17:30 -0700

    no issue id. Added the ToneExtensions class.

---
+++ src/Intercode.MusicLib/ToneExtensions.cs
@@ -0,0 +1,34 @@
+// 
+//   ToneExtensions.cs: 
+// 
+//   Author: Eddie Velasquez
+// 
+//   Copyright (c) 2013  Intercode Consulting, LLC.  All Rights Reserved.
+// 
+//      Unauthorized use, duplication or distribution of this software, 
+//      or any portion of it, is prohibited.  
+// 
+//   http://www.intercodeconsulting.com
+// 
+
+namespace Intercode.MusicLib
+{
+   public static class ToneExtensions
+   {
+      public static Tone Next(this Tone tone)
+      {
+         if( tone == Tone.B )
+            return Tone.C;
+
+         return tone + 1;
+      }
+
+      public static Tone Previous(this Tone tone)
+      {
+         if( tone == Tone.C )
+            return Tone.B;
+
+         return tone - 1;
+      }
+   }
+}
