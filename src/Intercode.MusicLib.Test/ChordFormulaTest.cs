dd5c866609d1ebf5d0de46629dd0fe8366427897
Eddie Velasquez <evelasquez@intercodeconsulting.com>
2013-09-13 10:04:04 -0700

    no issue id. Added the ChordFormula class and unit tests.

---
+++ src/Intercode.MusicLib.Test/ChordFormulaTest.cs
@@ -0,0 +1,127 @@
+﻿// 
+//   ChordFormulaTest.cs: 
+// 
+//   Author: Eddie Velasquez
+// 
+//   Copyright (c) 2013  Intercode Consulting, LLC.  All Rights Reserved.
+// 
+//      Unauthorized use, duplication or distribution of this software, 
+//      or any portion of it, is prohibited.  
+// 
+//   http://www.intercodeconsulting.com
+// 
+
+namespace Intercode.MusicLib.Test
+{
+   using System;
+   using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+   /// <summary>
+   ///    This is a test class for ChordFormulaTest and is intended
+   ///    to contain all ChordFormulaTest Unit Tests
+   /// </summary>
+   [ TestClass ]
+   public class ChordFormulaTest
+   {
+      #region Properties
+
+      /// <summary>
+      ///    Gets or sets the test context which provides
+      ///    information about and functionality for the current test run.
+      /// </summary>
+      public TestContext TestContext { get; set; }
+
+      #endregion
+
+      /// <summary>
+      ///    A test for ChordFormula Constructor
+      /// </summary>
+      [ TestMethod ]
+      public void ChordFormulaConstructorTest()
+      {
+         const string NAME = "Name";
+         const string SYMBOL = "Symbol";
+         const string FORMULA = "1,2,3";
+         var target = new ChordFormula(NAME, SYMBOL, FORMULA);
+
+         Assert.AreEqual(NAME, target.Name);
+         Assert.AreEqual(SYMBOL, target.Symbol);
+         Assert.IsNotNull(target.Formula);
+         Assert.AreEqual("1,2,3", target.Formula.ToString());
+      }
+
+      [ TestMethod ]
+      public void EqualsContractTest()
+      {
+         object x = new ChordFormula("Name", "Symbol", "1,2,3");
+         object y = new ChordFormula("Name", "Symbol", "1,2,3");
+         object z = new ChordFormula("Name", "Symbol", "1,2,3");
+
+         Assert.IsTrue(x.Equals(x)); // Reflexive
+         Assert.IsTrue(x.Equals(y)); // Symetric
+         Assert.IsTrue(y.Equals(x));
+         Assert.IsTrue(y.Equals(z)); // Transitive
+         Assert.IsTrue(x.Equals(z));
+         Assert.IsFalse(x.Equals(null)); // Never equal to null
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsContractTest()
+      {
+         var x = new ChordFormula("Name", "Symbol", "1,2,3");
+         var y = new ChordFormula("Name", "Symbol", "1,2,3");
+         var z = new ChordFormula("Name", "Symbol", "1,2,3");
+
+         Assert.IsTrue(x.Equals(x)); // Reflexive
+         Assert.IsTrue(x.Equals(y)); // Symetric
+         Assert.IsTrue(y.Equals(x));
+         Assert.IsTrue(y.Equals(z)); // Transitive
+         Assert.IsTrue(x.Equals(z));
+         Assert.IsFalse(x.Equals(null)); // Never equal to null
+      }
+
+      [ TestMethod ]
+      public void EqualsFailsWithDifferentTypeTest()
+      {
+         object actual = new ChordFormula("Name", "Symbol", "1,2,3");
+         Assert.IsFalse(actual.Equals(Int32.MinValue));
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsFailsWithDifferentTypeTest()
+      {
+         var actual = new ChordFormula("Name", "Symbol", "1,2,3");
+         Assert.IsFalse(actual.Equals(Int32.MinValue));
+      }
+
+      [ TestMethod ]
+      public void EqualsFailsWithNullTest()
+      {
+         object actual = new ChordFormula("Name", "Symbol", "1,2,3");
+         Assert.IsFalse(actual.Equals(null));
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsFailsWithNullTest()
+      {
+         var actual = new ChordFormula("Name", "Symbol", "1,2,3");
+         Assert.IsFalse(actual.Equals(null));
+      }
+
+      [ TestMethod ]
+      public void EqualsSucceedsWithSameObjectTest()
+      {
+         var actual = new ChordFormula("Name", "Symbol", "1,2,3");
+         Assert.IsTrue(actual.Equals(actual));
+      }
+
+      [ TestMethod ]
+      public void GetHashcodeTest()
+      {
+         var actual = new ChordFormula("Name", "Symbol", "1,2,3");
+         var expected = new ChordFormula("Name", "Symbol", "1,2,3");
+         Assert.IsTrue(expected.Equals(actual));
+         Assert.AreEqual(expected.GetHashCode(), actual.GetHashCode());
+      }
+   }
+}
---
+++ src/Intercode.MusicLib/ChordFormula.cs
@@ -0,0 +1,117 @@
+﻿// 
+//   ChordFormula.cs: 
+// 
+//   Author: Eddie Velasquez
+// 
+//   Copyright (c) 2013  Intercode Consulting, LLC.  All Rights Reserved.
+// 
+//      Unauthorized use, duplication or distribution of this software, 
+//      or any portion of it, is prohibited.  
+// 
+//   http://www.intercodeconsulting.com
+// 
+
+namespace Intercode.MusicLib
+{
+   using System;
+   using System.Diagnostics.Contracts;
+
+   public class ChordFormula: IEquatable<ChordFormula>
+   {
+      #region Constants
+
+      public static readonly ChordFormula Major = new ChordFormula("Major", "", "1,3,5");
+      public static readonly ChordFormula Major7 = new ChordFormula("Major Seventh", "M7", "1,3,5,7");
+      public static readonly ChordFormula Major9 = new ChordFormula("Major Ninth", "M9", "1,3,5,7,9");
+      public static readonly ChordFormula Major11 = new ChordFormula("Major Eleventh", "M11", "1,3,5,7,9,11");
+      public static readonly ChordFormula Major13 = new ChordFormula("Major Thirteenth", "M13", "1,3,5,7,9,11,13");
+      public static readonly ChordFormula Minor = new ChordFormula("Minor", "m", "1,3b,5");
+      public static readonly ChordFormula Minor7 = new ChordFormula("Minor Seventh", "m7", "1,3b,5,7b");
+      public static readonly ChordFormula Minor9 = new ChordFormula("Minor Ninth", "m9", "1,3b,5,7b,9");
+      public static readonly ChordFormula Minor11 = new ChordFormula("Minor Eleventh", "m11", "1,3b,5,7b,9,11");
+      public static readonly ChordFormula Minor13 = new ChordFormula("Minor Thirteenth", "m13", "1,3b,5,7b,9,11,13");
+      public static readonly ChordFormula Dominant7 = new ChordFormula("Dominant Seventh", "7", "1,3,5,7b");
+      public static readonly ChordFormula Dominant9 = new ChordFormula("Dominant Ninth", "9", "1,3,5,7b,9");
+      public static readonly ChordFormula Dominant11 = new ChordFormula("Dominant Eleventh", "11", "1,3,5,7b,9,11");
+      public static readonly ChordFormula Dominant13 = new ChordFormula("Dominant Thirteenth", "13", "1,3,5,7b,9,11,13");
+      public static readonly ChordFormula SixNine = new ChordFormula("Six Nine", "6/9", "1,3,5,6,9");
+      public static readonly ChordFormula AddNine = new ChordFormula("Add Nine", "add9", "1,3,5,9");
+      public static readonly ChordFormula Diminished = new ChordFormula("Diminished", "dim", "1,3b,5b");
+      public static readonly ChordFormula Diminished7 = new ChordFormula("Diminished Seventh", "dim7", "1,3b,5b,7bb");
+      public static readonly ChordFormula HalfDiminished = new ChordFormula("Half Diminished", "7dim5", "1,3b,5b,7b");
+      public static readonly ChordFormula Augmented = new ChordFormula("Augmented", "aug", "1,3,b#");
+      private static readonly StringComparer s_comparer = StringComparer.CurrentCulture;
+
+      #endregion
+
+      #region Construction
+
+      public ChordFormula(string name, string symbol, Formula formula)
+      {
+         Contract.Requires<ArgumentNullException>(name != null, "name");
+         Contract.Requires<ArgumentException>(name.Length > 0, "name");
+         Contract.Requires<ArgumentNullException>(symbol != null, "symbol");
+         Contract.Requires<ArgumentNullException>(formula != null, "formula");
+
+         Name = name;
+         Symbol = symbol;
+         Formula = formula;
+      }
+
+      public ChordFormula(string name, string symbol, string formula)
+         : this(name, symbol, Formula.Parse(formula))
+      {
+      }
+
+      #endregion
+
+      #region Properties
+
+      public string Name { get; private set; }
+      public string Symbol { get; private set; }
+      public Formula Formula { get; private set; }
+
+      #endregion
+
+      #region IEquatable<ChordFormula> Implementation
+
+      public bool Equals(ChordFormula other)
+      {
+         if( ReferenceEquals(other, this) )
+            return true;
+
+         if( ReferenceEquals(other, null) )
+            return false;
+
+         return s_comparer.Equals(Name, other.Name) && s_comparer.Equals(Symbol, other.Symbol)
+                && Formula.Equals(other.Formula);
+      }
+
+      public override bool Equals(object other)
+      {
+         if( ReferenceEquals(other, this) )
+            return true;
+
+         if( ReferenceEquals(other, null) || other.GetType() != GetType() )
+            return false;
+
+         return Equals((ChordFormula)other);
+      }
+
+      public override int GetHashCode()
+      {
+         return Name.GetHashCode();
+      }
+
+      #endregion
+
+      #region Overrides
+
+      public override string ToString()
+      {
+         return Name;
+      }
+
+      #endregion
+   }
+}
