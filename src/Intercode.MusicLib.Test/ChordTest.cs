ad163a7e14387ad90fc84f8f6756c44e8b0aef88
Eddie Velasquez <evelasquez@intercodeconsulting.com>
2013-09-13 10:21:18 -0700

    no issue id. Added the Chord class and unit tests.

---
+++ src/Intercode.MusicLib.Test/ChordTest.cs
@@ -0,0 +1,127 @@
+﻿// 
+//   ChordTest.cs: 
+// 
+//   Author: Eddie Velasquez
+// 
+//   Copyright (c) 2013  Intercode Consulting, LLC.  All Rights Reserved.
+// 
+//      Unauthorized use, duplication or distribution of this software, 
+//      or any portion of it, is prohibited.  
+// 
+//   http://www.intercodeconsulting.com
+// 
+
+namespace Intercode.MusicLib.Test
+{
+   using System;
+   using System.Linq;
+   using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+   /// <summary>
+   ///    This is a test class for ChordTest and is intended
+   ///    to contain all ChordTest Unit Tests
+   /// </summary>
+   [ TestClass ]
+   public class ChordTest
+   {
+      #region Properties
+
+      /// <summary>
+      ///    Gets or sets the test context which provides
+      ///    information about and functionality for the current test run.
+      /// </summary>
+      public TestContext TestContext { get; set; }
+
+      #endregion
+
+      /// <summary>
+      ///    A test for Chord Constructor
+      /// </summary>
+      [ TestMethod ]
+      public void ChordConstructorTest()
+      {
+         Note root = Note.Parse("C4");
+         ChordFormula formula = ChordFormula.Minor;
+         var target = new Chord(root, formula);
+
+         Assert.AreEqual(root, target.Root);
+         Assert.AreEqual(formula, target.Formula);
+         Assert.AreEqual("Cm", target.Name);
+         CollectionAssert.AreEqual(Note.ParseArray("C,Eb,G").ToArray(), target.Notes);
+      }
+
+      [ TestMethod ]
+      public void EqualsContractTest()
+      {
+         object x = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         object y = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         object z = new Chord(Note.Parse("C4"), ChordFormula.Major);
+
+         Assert.IsTrue(x.Equals(x)); // Reflexive
+         Assert.IsTrue(x.Equals(y)); // Symetric
+         Assert.IsTrue(y.Equals(x));
+         Assert.IsTrue(y.Equals(z)); // Transitive
+         Assert.IsTrue(x.Equals(z));
+         Assert.IsFalse(x.Equals(null)); // Never equal to null
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsContractTest()
+      {
+         var x = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         var y = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         var z = new Chord(Note.Parse("C4"), ChordFormula.Major);
+
+         Assert.IsTrue(x.Equals(x)); // Reflexive
+         Assert.IsTrue(x.Equals(y)); // Symetric
+         Assert.IsTrue(y.Equals(x));
+         Assert.IsTrue(y.Equals(z)); // Transitive
+         Assert.IsTrue(x.Equals(z));
+         Assert.IsFalse(x.Equals(null)); // Never equal to null
+      }
+
+      [ TestMethod ]
+      public void EqualsFailsWithDifferentTypeTest()
+      {
+         object actual = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         Assert.IsFalse(actual.Equals(Int32.MinValue));
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsFailsWithDifferentTypeTest()
+      {
+         var actual = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         Assert.IsFalse(actual.Equals(Int32.MinValue));
+      }
+
+      [ TestMethod ]
+      public void EqualsFailsWithNullTest()
+      {
+         object actual = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         Assert.IsFalse(actual.Equals(null));
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsFailsWithNullTest()
+      {
+         var actual = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         Assert.IsFalse(actual.Equals(null));
+      }
+
+      [ TestMethod ]
+      public void EqualsSucceedsWithSameObjectTest()
+      {
+         var actual = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         Assert.IsTrue(actual.Equals(actual));
+      }
+
+      [ TestMethod ]
+      public void GetHashcodeTest()
+      {
+         var actual = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         var expected = new Chord(Note.Parse("C4"), ChordFormula.Major);
+         Assert.IsTrue(expected.Equals(actual));
+         Assert.AreEqual(expected.GetHashCode(), actual.GetHashCode());
+      }
+   }
+}
---
+++ src/Intercode.MusicLib/Chord.cs
@@ -0,0 +1,107 @@
+﻿// 
+//   Chord.cs: 
+// 
+//   Author: Eddie Velasquez
+// 
+//   Copyright (c) 2013  Intercode Consulting, LLC.  All Rights Reserved.
+// 
+//      Unauthorized use, duplication or distribution of this software, 
+//      or any portion of it, is prohibited.  
+// 
+//   http://www.intercodeconsulting.com
+// 
+
+namespace Intercode.MusicLib
+{
+   using System;
+   using System.Diagnostics.Contracts;
+   using System.Linq;
+   using System.Text;
+
+   public class Chord: IEquatable<Chord>
+   {
+      #region Construction
+
+      public Chord(Note root, ChordFormula formula)
+      {
+         Contract.Requires<ArgumentNullException>(root != null, "root");
+         Contract.Requires<ArgumentNullException>(formula != null, "formula");
+
+         Root = root;
+         Formula = formula;
+
+         var buf = new StringBuilder();
+         buf.Append(root.Tone);
+         buf.Append(root.Accidental.ToSymbol());
+         buf.Append(formula.Symbol);
+
+         Name = buf.ToString();
+         Notes = Scale.Generate(root, formula.Formula).ToArray();
+      }
+
+      #endregion
+
+      #region Properties
+
+      public Note Root { get; private set; }
+      public string Name { get; private set; }
+      public ChordFormula Formula { get; private set; }
+      public Note[] Notes { get; private set; }
+
+      #endregion
+
+      #region IEquatable<Chord> Implementation
+
+      public bool Equals(Chord other)
+      {
+         if( ReferenceEquals(other, this) )
+            return true;
+
+         if( ReferenceEquals(other, null) )
+            return false;
+
+         return Root.Equals(other.Root) && Formula.Equals(other.Formula);
+      }
+
+      public override bool Equals(object other)
+      {
+         if( ReferenceEquals(other, this) )
+            return true;
+
+         if( ReferenceEquals(other, null) || other.GetType() != GetType() )
+            return false;
+
+         return Equals((Chord)other);
+      }
+
+      public override int GetHashCode()
+      {
+         int hashCode = Root.GetHashCode() ^ Formula.GetHashCode();
+         return hashCode;
+      }
+
+      #endregion
+
+      #region Overrides
+
+      public override string ToString()
+      {
+         var buf = new StringBuilder();
+         bool needsComma = false;
+
+         foreach( var note in Notes )
+         {
+            if( needsComma )
+               buf.Append(',');
+            else
+               needsComma = true;
+
+            buf.Append(note);
+         }
+
+         return buf.ToString();
+      }
+
+      #endregion
+   }
+}
