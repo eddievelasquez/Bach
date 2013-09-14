5ce3ec1e66e39b3c05ef87a5bf656ccf6eec3579
Eddie Velasquez <evelasquez@intercodeconsulting.com>
2013-09-14 14:35:58 -0700

    no issue id. Implemented the Mode class and unit tests.

---
+++ src/Intercode.MusicLib.Test/ModeTest.cs
@@ -0,0 +1,133 @@
+﻿namespace Intercode.MusicLib.Test
+{
+   using System;
+   using System.Linq;
+   using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+   /// <summary>
+   ///    This is a test class for ChordTest and is intended
+   ///    to contain all ChordTest Unit Tests
+   /// </summary>
+   [ TestClass ]
+   public class ModeTest
+   {
+      #region Properties
+
+      /// <summary>
+      ///    Gets or sets the test context which provides
+      ///    information about and functionality for the current test run.
+      /// </summary>
+      public TestContext TestContext { get; set; }
+
+      #endregion
+
+      /// <summary>
+      ///    A test for Mode Constructor
+      /// </summary>
+      [ TestMethod ]
+      public void ModeConstructorTest()
+      {
+         Note root = Note.Parse("C4");
+         ModeFormula formula = ModeFormula.Phrygian;
+         var target = new Mode(root, formula);
+
+         Assert.AreEqual(root, target.Root);
+         Assert.AreEqual(formula, target.Formula);
+         Assert.AreEqual("C Phrygian", target.Name);
+         CollectionAssert.AreEqual(Note.ParseArray("E4,F4,G4,A4,B4,C5,D5").ToArray(), target.ToArray());
+      }
+
+      private static void TestMode(string expectedNotes, Note root, ModeFormula formula)
+      {
+         var expected = Note.ParseArray(expectedNotes).ToArray();
+         CollectionAssert.AreEqual(expected, new Mode(root, formula).Take(expected.Length).ToArray());
+      }
+
+      [ TestMethod ]
+      public void ModesTest()
+      {
+         Note root = Note.Parse("C4");
+         TestMode("C4,D4,E4,F4,G4,A4,B4", root, ModeFormula.Ionian);
+         TestMode("D4,E4,F4,G4,A4,B4,C5", root, ModeFormula.Dorian);
+         TestMode("E4,F4,G4,A4,B4,C5,D5", root, ModeFormula.Phrygian);
+         TestMode("F4,G4,A4,B4,C5,D5,E5", root, ModeFormula.Lydian);
+         TestMode("G4,A4,B4,C5,D5,E5,F5", root, ModeFormula.Mixolydian);
+         TestMode("A4,B4,C5,D5,E5,F5,G5", root, ModeFormula.Aeolian);
+         TestMode("B4,C5,D5,E5,F5,G5,A5", root, ModeFormula.Locrian);
+      }
+
+      [ TestMethod ]
+      public void EqualsContractTest()
+      {
+         object x = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         object y = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         object z = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+
+         Assert.IsTrue(x.Equals(x)); // Reflexive
+         Assert.IsTrue(x.Equals(y)); // Symetric
+         Assert.IsTrue(y.Equals(x));
+         Assert.IsTrue(y.Equals(z)); // Transitive
+         Assert.IsTrue(x.Equals(z));
+         Assert.IsFalse(x.Equals(null)); // Never equal to null
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsContractTest()
+      {
+         var x = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         var y = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         var z = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+
+         Assert.IsTrue(x.Equals(x)); // Reflexive
+         Assert.IsTrue(x.Equals(y)); // Symetric
+         Assert.IsTrue(y.Equals(x));
+         Assert.IsTrue(y.Equals(z)); // Transitive
+         Assert.IsTrue(x.Equals(z));
+         Assert.IsFalse(x.Equals(null)); // Never equal to null
+      }
+
+      [ TestMethod ]
+      public void EqualsFailsWithDifferentTypeTest()
+      {
+         object actual = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         Assert.IsFalse(actual.Equals(Int32.MinValue));
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsFailsWithDifferentTypeTest()
+      {
+         var actual = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         Assert.IsFalse(actual.Equals(Int32.MinValue));
+      }
+
+      [ TestMethod ]
+      public void EqualsFailsWithNullTest()
+      {
+         object actual = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         Assert.IsFalse(actual.Equals(null));
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsFailsWithNullTest()
+      {
+         var actual = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         Assert.IsFalse(actual.Equals(null));
+      }
+
+      [ TestMethod ]
+      public void EqualsSucceedsWithSameObjectTest()
+      {
+         var actual = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         Assert.IsTrue(actual.Equals(actual));
+      }
+
+      [ TestMethod ]
+      public void GetHashcodeTest()
+      {
+         var actual = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         var expected = new Mode(Note.Parse("C4"), ModeFormula.Dorian);
+         Assert.IsTrue(expected.Equals(actual));
+         Assert.AreEqual(expected.GetHashCode(), actual.GetHashCode());
+      }
+   }
+}
---
+++ src/Intercode.MusicLib/Mode.cs
@@ -0,0 +1,125 @@
+﻿// 
+//   Mode.cs: 
+// 
+//   Author: Eddie Velasquez
+// 
+//   Copyright (c) 2013  Intercode Consulting, LLC.  All Rights Reserved.
+// 
+//      Unauthorized use, duplication or distribution of this software, 
+//      or any portion of it, is prohibited.  
+// 
+//   http://www.intercodeconsulting.com
+// 
+
+namespace Intercode.MusicLib
+{
+   using System;
+   using System.Collections;
+   using System.Collections.Generic;
+   using System.Diagnostics.Contracts;
+   using System.Linq;
+   using System.Text;
+
+   public class Mode: IEquatable<Mode>, IEnumerable<Note>
+   {
+      #region Construction
+
+      public Mode(Note root, ModeFormula formula)
+      {
+         Contract.Requires<ArgumentNullException>(root != null, "root");
+         Contract.Requires<ArgumentNullException>(formula != null, "formula");
+
+         Root = root;
+         Formula = formula;
+
+         var buf = new StringBuilder();
+         buf.Append(root.Tone);
+         buf.Append(root.Accidental.ToSymbol());
+         buf.Append(' ');
+         buf.Append(formula.Name);
+
+         Name = buf.ToString();
+      }
+
+      #endregion
+
+      #region Properties
+
+      public Note Root { get; private set; }
+      public string Name { get; private set; }
+      public ModeFormula Formula { get; private set; }
+
+      #endregion
+
+      #region IEquatable<Mode> Implementation
+
+      public bool Equals(Mode other)
+      {
+         if( ReferenceEquals(other, this) )
+            return true;
+
+         if( ReferenceEquals(other, null) )
+            return false;
+
+         return Root.Equals(other.Root) && Formula.Equals(other.Formula);
+      }
+
+      public override bool Equals(object other)
+      {
+         if( ReferenceEquals(other, this) )
+            return true;
+
+         if( ReferenceEquals(other, null) || other.GetType() != GetType() )
+            return false;
+
+         return Equals((Mode)other);
+      }
+
+      public override int GetHashCode()
+      {
+         int hashCode = Root.GetHashCode() ^ Formula.GetHashCode();
+         return hashCode;
+      }
+
+      #endregion
+
+      #region IEnumerable<Note> Implementation
+
+      IEnumerator IEnumerable.GetEnumerator()
+      {
+         return GetEnumerator();
+      }
+
+      public IEnumerator<Note> GetEnumerator()
+      {
+         var major = ScaleFormula.Major;
+         var count = major.Count;
+         Note[] notes = new Scale(Root, major).Skip(Formula.Tonic - 1).Take(count).ToArray();
+         return ((IEnumerable<Note>)notes).GetEnumerator();
+      }
+
+      #endregion
+
+      #region Overrides
+
+      public override string ToString()
+      {
+         var buf = new StringBuilder();
+         bool needsComma = false;
+
+         foreach( var note in this )
+         {
+            if( needsComma )
+               buf.Append(',');
+            else
+               needsComma = true;
+
+            buf.Append(note);
+         }
+
+         return buf.ToString();
+      }
+
+      #endregion
+   }
+}
