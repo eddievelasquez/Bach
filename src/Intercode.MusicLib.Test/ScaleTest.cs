21e8af7b825ac3f3987fab5f9055dc2f42c54059
Eddie Velasquez <evelasquez@intercodeconsulting.com>
2013-09-14 07:39:30 -0700

    no issue id. Added IEquatable<> unit testing for the Scale class.

--- src/Intercode.MusicLib.Test/ScaleTest.cs
+++ src/Intercode.MusicLib.Test/ScaleTest.cs
@@ -13,6 +13,7 @@
 
 namespace Intercode.MusicLib.Test
 {
+   using System;
    using System.Linq;
    using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -46,6 +47,80 @@ namespace Intercode.MusicLib.Test
          TestScale("C,D,E,G,A", root, ScaleFormula.Pentatonic);
          TestScale("C,Eb,F,Gb,G,Bb", root, ScaleFormula.Blues);
          TestScale("C,D,Eb,E,G,A", root, ScaleFormula.Gospel);
+      }
+
+      [ TestMethod ]
+      public void EqualsContractTest()
+      {
+         object x = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         object y = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         object z = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+
+         Assert.IsTrue(x.Equals(x)); // Reflexive
+         Assert.IsTrue(x.Equals(y)); // Symetric
+         Assert.IsTrue(y.Equals(x));
+         Assert.IsTrue(y.Equals(z)); // Transitive
+         Assert.IsTrue(x.Equals(z));
+         Assert.IsFalse(x.Equals(null)); // Never equal to null
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsContractTest()
+      {
+         var x = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         var y = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         var z = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+
+         Assert.IsTrue(x.Equals(x)); // Reflexive
+         Assert.IsTrue(x.Equals(y)); // Symetric
+         Assert.IsTrue(y.Equals(x));
+         Assert.IsTrue(y.Equals(z)); // Transitive
+         Assert.IsTrue(x.Equals(z));
+         Assert.IsFalse(x.Equals(null)); // Never equal to null
+      }
+
+      [ TestMethod ]
+      public void EqualsFailsWithDifferentTypeTest()
+      {
+         object actual = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         Assert.IsFalse(actual.Equals(Int32.MinValue));
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsFailsWithDifferentTypeTest()
+      {
+         var actual = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         Assert.IsFalse(actual.Equals(Int32.MinValue));
+      }
+
+      [ TestMethod ]
+      public void EqualsFailsWithNullTest()
+      {
+         object actual = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         Assert.IsFalse(actual.Equals(null));
+      }
+
+      [ TestMethod ]
+      public void TypeSafeEqualsFailsWithNullTest()
+      {
+         var actual = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         Assert.IsFalse(actual.Equals(null));
+      }
+
+      [ TestMethod ]
+      public void EqualsSucceedsWithSameObjectTest()
+      {
+         var actual = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         Assert.IsTrue(actual.Equals(actual));
+      }
+
+      [ TestMethod ]
+      public void GetHashcodeTest()
+      {
+         var actual = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         var expected = new Scale(Note.Parse("C4"), ScaleFormula.Major);
+         Assert.IsTrue(expected.Equals(actual));
+         Assert.AreEqual(expected.GetHashCode(), actual.GetHashCode());
       }
    }
 }
