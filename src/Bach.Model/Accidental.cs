bc028eedd76549767a6f3bab865cf4f76dc3c6eb
Eddie Velasquez <evelasquez@intercodeconsulting.com>
2016-04-02 11:05:34 -0700

    Renamed the Intercode.* directories to Bach.*

---
+++ src/Bach.Model/Accidental.cs
@@ -0,0 +1,24 @@
+// 
+//   Accidental.cs: 
+// 
+//   Author: Eddie Velasquez
+// 
+//   Copyright (c) 2013  Intercode Consulting, LLC.  All Rights Reserved.
+// 
+//      Unauthorized use, duplication or distribution of this software, 
+//      or any portion of it, is prohibited.  
+// 
+//   http://www.intercodeconsulting.com
+// 
+
+namespace Bach.Model
+{
+   public enum Accidental
+   {
+      DoubleFlat = -2,
+      Flat = -1,
+      Natural = 0,
+      Sharp = 1,
+      DoubleSharp = 2
+   }
+}
